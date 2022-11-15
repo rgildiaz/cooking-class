bea1b6e07485da8e82c8cd1018923a6f32a207de
Rafi Gil Diaz <rfgildiaz@gmail.com>
2022-11-14 21:19:37 -0600

    Add footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "../Header";
 import Landing from "../Landing";
+import Footer from "../Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <em>small quote here</em>
         </p>
       </Landing>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+function Footer(props) {
+    return (
+        <footer>
+            &copy; Lisa Gil Diaz 2022
+        </footer>
+    )
+}
+
+export default Footer;
