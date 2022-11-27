af69cb3cf8805e0539e97b85f7bc81b8faaca8b8
Rafi Gil Diaz <rfgildiaz@gmail.com>
2022-11-27 13:56:29 -0600

    sign-up page google form

--- frontend/src/components/pages/Join/Join.js
+++ frontend/src/components/pages/Join/Join.js
@@ -1,9 +1,24 @@
+import "./Join.css";
+
 const Join = (props) => {
-    return (
-        <div id="join" className="page">
-            <h1>Sign up</h1>
-        </div>
-      );
-}
+  return (
+    <div id="join" className="page">
+      <h1>Sign up</h1>
+      <div className="content">
+        <iframe
+          title="form"
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfyffHVRnV6f9wll8prRshtxz7GN7JZZqlasrewgTz7fFJLQw/viewform?embedded=true"
+          width="640"
+          height="700"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
+      </div>
+    </div>
+  );
+};
 
 export default Join;
